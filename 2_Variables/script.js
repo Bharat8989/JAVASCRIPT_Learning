cd54fbc7f6bbcc525c055dece478d5411ce9c0c2
Bharat <kadamb208@gmail.com>
2024-06-23 22:25:36 +0530

    adding files variables

---
+++ 2_Variables/script.js
@@ -0,0 +1,20 @@
+let username="Bharat kadam";
+let userInto="my name is bharat kadam ";
+let userE="what is your name and equations"
+let isHappy="true"
+let fatherName="Nagnath kadam"
+let age=23
+username=100;
+age=23;
+let userNamee='hi i am '+username +' my father name ' + fatherName + ' I am ' +age + ' years old.';
+
+// const variable 
+const yourName='bharat'
+const hoursInday=24
+// hoursInday=23
+
+
+// var variable
+
+var let='king'
+console.log(username)
