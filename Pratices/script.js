ca815e47debce41ec016672948253820d90363ba
Bharat <kadamb208@gmail.com>
2024-06-23 22:26:34 +0530

    pratices files

---
+++ Pratices/script.js
@@ -0,0 +1,16 @@
+console.log('Hello world');
+// window.alert('Welcome to CodSoft internship');
+// alert('Welcome to my home');
+// document.write("adhira school is Awesome.");
+document.getElementById("div").innerHTML = 'No, it is more than just a simple div';
+document.getElementById('div2').innerHTML=13
+
+
+
+
+document.getElementsByClassName("h1")[0].innerHTML='Different ways to Print Output'
+document.getElementsByClassName('p1')[0].innerHTML="when it comes to javascript, there are four different ways with which we can print output to the terminal or console."
+document.getElementById('l1').innerHTML=' using console.log()'
+document.getElementById('2').innerHTML='using window.alter()'
+document.getElementById('3').innerHTML='using document.write()'
+document.getElementById('4').innerHTML='using innerHTML';
