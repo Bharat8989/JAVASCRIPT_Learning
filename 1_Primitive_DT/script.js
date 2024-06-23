ca69ba18d179118ce097ca2bc60e79e3c1b581d4
Bharat <kadamb208@gmail.com>
2024-06-23 17:01:30 +0530

    adding files

---
+++ 1_Primitive_DT/script.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log('Different Ways to print output')
+
+window.alert("Using window.alter()")
+document.write("using document.write()")
+// innerHTML("using inner")
+document.getElementById("simplediv").innerHTML="no,it is more than just a simple div"
+document.getElementById("simplediv").innerHTML=13
