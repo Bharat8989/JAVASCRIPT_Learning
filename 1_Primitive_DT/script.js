b0c7f6a412108bebcca390bf099d48733948f4bf
Bharat <kadamb208@gmail.com>
2024-06-24 18:39:09 +0530

    second commit

--- 1_Primitive_DT/script.js
+++ 1_Primitive_DT/script.js
@@ -1,3 +1,17 @@
+document.getElementById('h1').innerHTML='Two Types of Data Types'
+document.getElementById('h2').innerHTML='Primitive'
+document.getElementById('h3').innerHTML='Non Primitive'
+document.getElementById('h4').innerHTML='In this topic well learn about Primitive data types'
+document.getElementById('h5').innerHTML='We can classify Primitive data types in 7 categories'
+document.getElementById('h6').innerHTML='number'
+document.getElementById('h7').innerHTML='string'
+document.getElementById('h8').innerHTML='boolean'
+document.getElementById('h9').innerHTML='undefined'
+document.getElementById('h10').innerHTML='Null'
+document.getElementById('h11').innerHTML='bigint'
+document.getElementById('h12').innerHTML='symbol'
+
+
 console.log("hello world");
 console.log('Different Ways to print output')
 
