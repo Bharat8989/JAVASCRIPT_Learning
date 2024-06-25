b53a326760045cf4b5763cfc8379b0a04131dbd8
Bharat <kadamb208@gmail.com>
2024-06-25 10:50:49 +0530

    Comparison

---
+++ 6_Comparison/script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('h1')[0].innerHTML='Comparison Operators'
+let userAge1=10;
+let userAge2=20;
+let Comparison = userAge1==userAge2
+console.log(Comparison)
+document.write(Comparison)
