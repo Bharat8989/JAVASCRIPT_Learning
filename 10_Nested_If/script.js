fd35940e0ed650cdfa4315f91ce6daaf0284cf57
Bharat <kadamb208@gmail.com>
2024-06-25 17:07:58 +0530

    Nested if

---
+++ 10_Nested_If/script.js
@@ -0,0 +1,13 @@
+let name = "Masai School";
+let age = 25;
+let isMarried = false;
+
+if (name == "Masai School") {
+        if (age == 25) {
+                if (isMarried === false) {
+                        console.log("You are allowed to enter!");
+                }
+        }
+}
+
+console.log("Done!");
