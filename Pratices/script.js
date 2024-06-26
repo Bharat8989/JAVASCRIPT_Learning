07bfab56e87d8b473a5b76d586d2536e8a0b9e14
Bharat <kadamb208@gmail.com>
2024-06-26 17:56:14 +0530

    Array files add

--- Pratices/script.js
+++ Pratices/script.js
@@ -1,29 +1,24 @@
-document.getElementById('h1').innerHTML='Comparison Operators'
-document.getElementById('h11').innerHTML="Assignment operator"
+document.getElementById('h1').innerHTML = 'Arrays';
 
+const studentName=['Bharat', 'Suraj', 'Pavan', 'Mukinda', 'Datta', 'Atamaram','Kartik']
+console.log(studentName)
+document.write("Student List : <br>" +studentName + "<br>")
+studentName.push('manmit');
+document.getElementById('demo').innerHTML=studentName;
 
-let x=10,y=5;
-console.log("x and y are:"+x,+y)
-let result = x==y;
-console.log('x==y is:'+result)
+const person={
+    firstName:'john',lastName:'doe',age:23
+};
+let king=studentName.length;
+console.log(king)
+document.write(king);
 
-result = x!=y
-console.log(result)
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits[0];
 
-result = x===y
-console.log(result)
+//new array
 
-result = x!==y
-console.log(result)
-
-result = x>y
-console.log(result)
-
-result = x>=y
-console.log(result)
-
-result = x<y
-console.log(result)
-
-result = x<=y
-console.log(result)
+const points=new Array(23,43,543,23,12,34,45);
+const point=[23,43,23,45,67,87,55];
+console.log(points);
+console.log(point);
