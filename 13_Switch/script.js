4b05e6b2e38527d1aa91093de2d229d6f674a70e
Bharat <kadamb208@gmail.com>
2024-06-27 10:56:42 +0530

    adding Switch

---
+++ 13_Switch/script.js
@@ -0,0 +1,30 @@
+document.getElementById('k').innerHTML="Break Statement with Switch case in javascript"
+
+let day =8;
+switch(day)
+{
+    case 1:
+        console.log("sunday");
+        break;
+
+    case 2:
+        console.log("monday");
+        break;
+        case 3:
+            console.log("tuesday");
+            break;
+            case 4:
+        console.log("Wednesday");
+        break;
+        case 5:
+        console.log("thursday");
+        break;
+        case 6:
+        console.log("friday");
+        break;
+        case 7:
+        console.log("saturday");
+        break;    
+        default:
+            console.log("wrong day");
+}        
