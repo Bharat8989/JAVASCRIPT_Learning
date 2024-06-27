bb138619296a98afdccf7816d3f1577fee451d23
Bharat <kadamb208@gmail.com>
2024-06-27 10:56:03 +0530

    adding while Loop

---
+++ 12_While_loop/script.js
@@ -0,0 +1,22 @@
+document.getElementsByClassName('h')[0].innerHTML='While Loop'
+
+//while loop
+let i=0;
+while(i<=10){
+    console.log(i)
+    document.write("<br>"+i)
+i++
+}
+
+//do while loop
+let k=1;
+do{
+    console.log(k)
+    document.write("<br>" +k)
+    k++;
+}while(k<=15);
+
+// let p=1;
+// while(p<=5){
+//     console.log(p);
+// }
