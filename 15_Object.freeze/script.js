3792aa03e21c4994336c3b0d4c03ee9b4bc4225a
Bharat <kadamb208@gmail.com>
2024-06-27 10:59:18 +0530

    adding 15_Object.freeze

---
+++ 15_Object.freeze/script.js
@@ -0,0 +1,17 @@
+const user={
+    firstName:'Bharat',
+    lastName:'Kadam',
+    pata:{
+        city:'Beed',
+        pinCode:343211,
+        state:'Maharashtra',
+        moreDetails:{
+            populations:23423534,
+            area:'234 sq km',
+        },
+
+    },
+    age:16,
+    isGraduate:false,
+}
+Object.seal(user)
