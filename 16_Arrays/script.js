f8568675978a81b8a9dfe49a0c177c10d9bd6418
Bharat <kadamb208@gmail.com>
2024-06-28 18:32:14 +0530

    files add

---
+++ 16_Arrays/script.js
@@ -0,0 +1,12 @@
+let userName='Bharat Kadam'
+
+
+
+const fruitsCollections=['apple','banana', 'grapes','date']
+fruitsCollections[3]='orange';
+fruitsCollections.push('sitafal')
+console.log(fruitsCollections);
+document.write("FruitsCollections:"+fruitsCollections);
+ 
+const newObjects={}
+const newArray=[]
