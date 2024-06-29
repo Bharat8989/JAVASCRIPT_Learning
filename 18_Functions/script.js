0d7fe735ced848e02e76c767b925eed86dbaa5ab
Bharat <kadamb208@gmail.com>
2024-06-29 10:57:24 +0530

    functions learing

---
+++ 18_Functions/script.js
@@ -0,0 +1,22 @@
+//functions Definition
+function myFunctions(username,profession,age){
+        // console.log(userName)
+        console.log(typeof username)
+        console.log(typeof profession)
+        console.log(typeof age)
+        console.log("hi,")
+        console.log(`My name is ${username} `)
+        console.log(`I am a ${profession}`)
+        console.log(`he is ${age} years old`)
+        // return true;
+}
+//         functions call
+// const my=myFunctions();
+// console.log(my)
+// const returnValue=myFunctions();
+// console.log("****************")
+// myFunctions()
+myFunctions('Bharat kadam' ,'software Engineer',23)
+myFunctions('suraj kadam', 'Mechanical Engineer',16)
+myFunctions();
+
