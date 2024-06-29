c28589e5a4a65029dafd2ea457199e4f2dc82e00
Bharat <kadamb208@gmail.com>
2024-06-29 18:33:52 +0530

    call stack add files

---
+++ 21_Call_Stack/script.js
@@ -0,0 +1,19 @@
+
+
+sayHi()
+const username='Bharat'
+const lastname='Kadam'
+console.log(username)
+console.log(lastname)
+
+function sayHi(){
+    console.log('say hi')
+}
+
+function introduceMe()
+{
+    console.log('hi , my name is Bharat')
+    introduceMe()
+}
+introduceMe()
+console.log('program ended')
