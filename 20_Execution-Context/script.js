b09574dfe857bcdb8f6a294ea4c3e816716549e0
Bharat <kadamb208@gmail.com>
2024-06-29 18:33:13 +0530

    execution files add

---
+++ 20_Execution-Context/script.js
@@ -0,0 +1,14 @@
+debugger
+const userName='Bharat'
+const userAge=21;
+console.log(userAge)
+console.log(userName)
+
+function sayHi(){
+    console.log('say Hi ')
+}
+sayHi()//call functions 
+// var age1=23;
+// var age2=24;
+// console.log(age1)
+// console.log(age2)
