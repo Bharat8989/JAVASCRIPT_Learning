49ab07ae6838216af4964eebdd7636a384d249c1
Bharat <kadamb208@gmail.com>
2024-07-01 08:53:31 +0530

    adding hoisting files

---
+++ 22_Hoisting/script.js
@@ -0,0 +1,18 @@
+console.log("Hoisting")
+//function definition 
+//functions declarations
+
+console.log(a);
+var a='Bharat';
+// console.log(a);
+
+hi()
+function hi(){
+    console.log("Hi , Bharat")
+}
+//function definition 
+//functions expression
+const sayHi=function(){
+    console.log("hi ,Suraj")
+}
+sayHi();
