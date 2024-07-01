2f9813ab57d3f4ad054aee8f342c6b1c7b7d8b71
Bharat <kadamb208@gmail.com>
2024-07-01 08:54:51 +0530

    adding files functions

---
+++ 24_Higher__Order_Functions/script.js
@@ -0,0 +1,14 @@
+function a(b){
+    console.log(typeof b)
+    console.log('hii');
+}
+
+a('hi my name is Bharat')
+a({userName:'bhaat', useerAge:32 } )
+
+a([2,3,4,23,2,3,])
+console.dir('what is ' );
+console.dir(34543)
+console.dir(a)
+// a.age=34;
+
