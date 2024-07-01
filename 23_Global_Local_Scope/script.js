e781f1cfe5d9e146c9fbd49472e18c4f9e961e52
Bharat <kadamb208@gmail.com>
2024-07-01 08:54:08 +0530

    23_Global_Local_Scope

---
+++ 23_Global_Local_Scope/script.js
@@ -0,0 +1,27 @@
+
+'use strict'
+var userName='Bharat'
+const userAge=21;
+
+function add(){
+    let userName='suraj'
+    const x=2;
+    const y=4;
+    console.log(x+y);
+    console.log(userName)
+}
+
+function subtract(){
+    
+    const x=13;
+    const y=10;
+    console.log(x-y)
+    console.log(userName)
+    function child(){
+        const childname='golu';
+        console.log(childname)
+    }
+    child()
+}
+add()
+subtract()
