8c4cf091c6c360453364a52a2619ff3a729836af
Bharat <kadamb208@gmail.com>
2024-07-01 08:55:41 +0530

    adding files 25_SetTimeout-SetInterval

---
+++ 25_SetTimeout-SetInterval/script.js
@@ -0,0 +1,21 @@
+// setTimeout('console.log(true)');
+// setTimeout('console.log("Bharat Kadam")',2000)
+// setInterval();
+setTimeout(`console.log("hi")
+    console.log("I am Bharat Kadam")`)
+
+const time=setTimeout(`console.log("hi-1")`,0)
+const time1=setTimeout(`console.log("hi-4")`,6000)
+const time3=setTimeout(a(),8000)
+
+
+//setinterval 
+const time4=setInterval(`console.log("hH-1")`,2000)
+// const time1=setTimeout(`console.log("hi-4")`,4000)
+const time5=setTimeout(a(),8000)
+// clearInterval(time3)
+
+
+function a(){
+    console.log("hello world")
+}
