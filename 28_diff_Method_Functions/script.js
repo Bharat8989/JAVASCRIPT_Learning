8d57fe8c02c5d9d025df04ddb170a2e3f47df4d6
Bharat <kadamb208@gmail.com>
2024-07-02 10:09:58 +0530

    28_diff_Method_Functions

---
+++ 28_diff_Method_Functions/script.js
@@ -0,0 +1,13 @@
+const maths={
+    E:2.3454334543,
+    add:function(a,b){
+        return a+b
+    },
+    square:function(num){
+        return num *num;
+    },
+    cube(num)
+    {
+        return num**3
+    }
+}
