fb324f4a02cd83c8f6198ba12d8a1900c8673233
Bharat <kadamb208@gmail.com>
2024-07-02 10:08:57 +0530

    event loop and queue

---
+++ 26_Event_loop_Queue/script.js
@@ -0,0 +1,23 @@
+console.log('Hi-1')
+
+function hello(){
+    console.log("hello world")
+}
+
+
+for (let i=0;i<=4;i++)
+    {
+        console.log(i)
+    }
+
+    hello();
+
+
+setTimeout(function(){
+    console.log('Hi-3')
+})
+
+// setTimeout('Hi-4');
+
+console.log("H1-2")
+
