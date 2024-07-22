3a4f90a1274229337d0ce1a269a8387d81dd51ea
Bharat <kadamb208@gmail.com>
2024-07-22 21:43:09 +0530

    adding 49

---
+++ 49_Import-Export/script.js
@@ -0,0 +1,23 @@
+// import { multiply as mul,add } from "./utility.js";
+
+
+// const result3=multiply(3,5,6)
+// const result4=mul(34,5,2)
+// const add1=add(3,4,3)
+// console.log(add1)
+// // console.log(result3)
+// console.log(result4)
+
+import * as utility from './utility.js';
+const result=utility.add(3,3,4)
+const result1=utility.multiply(34,3,2)
+console.log(result)
+console.log(result1)
+
+
+console.log(utility.studentCount)
+
+
+import  add from './utility.js'
+const result2=add(3,4,3)
+console.log(result2)
---
+++ 49_Import-Export/utility.js
@@ -0,0 +1,18 @@
+export function multiply(a,b,c){
+    return a*b*c;
+}
+
+function add(a,b,c){
+     return a+b+c;
+}
+export{
+    add as add,
+    
+}
+ 
+
+export const studentCount=34;
+
+export default function sub(a,b,c){
+    return a-b-c;
+}
